785015152315275af7320780986f86f02b77de55
AvranWong <avranwong@gmail.com>
2023-05-31 00:15:59 +0700

    skeleton version of bot

--- bot.py
+++ bot.py
@@ -1,21 +1,33 @@
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram import (
+    Update
+)
+from telegram.constants import ParseMode
+from telegram.ext import (
+    ApplicationBuilder, CommandHandler,CallbackContext,
+)
 from web3 import Web3
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+TELE_TOKEN = os.getenv("TELE_TOKEN")
+NODE_PROVIDER_ENDPOINT = os.getenv("NODE_PROVIDER_ENDPOINT")
+
+ROUTE, NEW_USER, NEW_USER_NAME, SHOW_QR = range(4)
 # Function to handle the /start command
-def start(update: Update, context: CallbackContext):
+async def start(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
     gas_fee, block_number = get_ethereum_data()
     message = f"Current Gas Fees: {gas_fee} gwei\nCurrent Block Number: {block_number}"
-    context.bot.send_message(chat_id=chat_id, text=message)
+    await context.bot.send_message(chat_id=chat_id, text=message)
 
 # Function to retrieve Ethereum data
 def get_ethereum_data():
     # Infura endpoint
-    infura_endpoint = "https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID"  # Replace with your Infura project ID
 
     # Create a web3 instance using the Infura endpoint
-    web3 = Web3(Web3.HTTPProvider(infura_endpoint))
+    web3 = Web3(Web3.HTTPProvider(NODE_PROVIDER_ENDPOINT))
 
     # Get the current gas price
     gas_price = web3.eth.gas_price
@@ -24,21 +36,18 @@ def get_ethereum_data():
     block_number = web3.eth.block_number
 
     # Convert gas price to gwei
-    gas_fee = web3.fromWei(gas_price, 'gwei')
+    gas_fee = web3.from_wei(gas_price, 'ether')
 
     return gas_fee, block_number
 
 def main():
-    # Initialize the Telegram bot
-    updater = Updater("YOUR_BOT_TOKEN")  # Replace with your own Telegram bot token
-    dispatcher = updater.dispatcher
+    app = ApplicationBuilder().token(TELE_TOKEN).build()
 
-    # Register the /start command handler
-    dispatcher.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("start", start))
 
-    # Start the bot
-    updater.start_polling()
-    updater.idle()
+    app.run_polling()
+    return
+
 
 if __name__ == '__main__':
     main()
